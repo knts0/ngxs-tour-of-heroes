74a1fbcd9ddab03e2ceea78d7f23714a3f3076d6
knts0 <shimmer22.0204@gmail.com>
2019-03-25 01:07:15 +0900

    refactor

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
 import { MessageService } from './message.service';
